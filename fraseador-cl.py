765a8ba6d8e2af15fc3d7dc28cdc9734e7987736
Andre Lima <andre.lima@gmail.com>
2013-12-24 18:30:51 -0200

    Added docopt to handle command-line args

--- fraseador-cl.py
+++ fraseador-cl.py
@@ -1,14 +1,35 @@
+"""Fraseador
+
+Usage:
+  fraseador-cl.py [-N NUM] [-L]
+  fraseador-cl.py (-h | --help)
+
+Options:
+  -h --help            Show this screen.
+  -v --version         Show version and exit.
+  -N NUM --number=NUM  Number of sentences to be generated [default: 20].
+  -L --lines           Break line after each sentence.
+
+"""
+from __future__ import print_function
+
+from docopt import docopt
+
 from syntax import clause
 
 
 def main():
-    n = 20
-    lf = '\n'
-    print lf.join([
-        repr(clause()).capitalize() + '.'
-        for i in range(n)
-    ])
+    arguments = docopt(__doc__, version='1.0.0rc2')
+    number = int(arguments.get('--number'))
+    lines = bool(arguments.get('--lines'))
+    end = '\n' if lines else ' '
 
+    for i in xrange(number):
+        sentence = repr(clause()).capitalize() + '.'
+        print(sentence, end=end)
+
+    if not lines:
+        print()
 
 if __name__ == '__main__':
     main()
